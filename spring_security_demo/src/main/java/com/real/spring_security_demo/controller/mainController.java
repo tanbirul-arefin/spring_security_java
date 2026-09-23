af52ff3c512be2d13cd11f11d9447b870433a6ca
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-23 10:14:43 +0600

    amra login page ar security niye kaj bujci

---
+++ spring_security_demo/src/main/java/com/real/spring_security_demo/controller/mainController.java
@@ -0,0 +1,23 @@
+package com.real.spring_security_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class mainController {
+
+//    @GetMapping("/login")
+//    public String loginPage(){
+//        return "login-page";
+//    }
+
+
+    @GetMapping("/protected")
+    public String protectedPage(){
+        return "protected-page";
+    }
+
+}
